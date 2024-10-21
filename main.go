47edb39e68b39be7179ecaeba0edd3c8bf168741
pman <pman-$-432894328923@gmail.com>
2024-10-21 22:52:10 +0200

    ...

--- main.go
+++ main.go
@@ -5,9 +5,43 @@ import (
 	"fmt"
 	"log"
 	"os"
+  "strings"
 )
 
 func main() {
+	// Read JSON data from the file
+	data, err := os.ReadFile("version-test.json")
+	if err != nil {
+		fmt.Println("Error reading file:", err)
+		return
+	}
+
+	// Convert the data to a string
+	content := string(data)
+
+	// Find the index of the first occurrence of '{'
+	index := strings.Index(content, "{")
+
+	// Check if '{' was found
+	if index == -1 {
+		log.Fatal("No opening brace found in the content.")
+		return
+	}
+
+	// Store the characters before '{' in a variable
+	beforeBrace := content[:index]
+	
+	// Extract the JSON string starting from '{'
+	jsonString := content[index:]
+
+	// Print the results
+	fmt.Println("Characters before '{':", beforeBrace)
+	fmt.Println("JSON String:", jsonString)
+}
+
+
+
+func updateVersion() {
 	jsonData, err := os.ReadFile("version.json")
 	if err != nil {
 		fmt.Println("Error opening file:", err)
