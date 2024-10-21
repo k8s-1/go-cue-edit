c3689e92201887821c174d0ddc39b6821a76575a
pman <pman-$-432894328923@gmail.com>
2024-10-21 22:33:46 +0200

    chore: update file(s): main.go

--- main.go
+++ main.go
@@ -1,18 +1,25 @@
 package main
 
 import (
-  "encoding/json"
-  "os"
-  "log"
+	"encoding/json"
+	"fmt"
+	"os"
 )
 
-// The data struct for the decoded data
-// Notice that all fields must be exportable!
-type Data struct {
-    Origin string
-    User   string
-    Active bool
-}
+// Define a map to capture the app versions
+var appVersions map[string]string
 
 func main() {
+	// Open the JSON file
+	jsonData, err := os.ReadFile("version.json")
+	if err != nil {
+		fmt.Println("Error opening file:", err)
+		return
+	}
+
+	err = json.Unmarshal(jsonData, &appVersions)
+	if err != nil {
+		fmt.Println("error:", err)
+	}
+	fmt.Printf("%+v", jsonData)
 }
