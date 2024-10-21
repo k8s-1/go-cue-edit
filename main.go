f70d2787345bec22d001700cf8292bcf5eafa522
pman <pman-$-432894328923@gmail.com>
2024-10-21 22:43:33 +0200

    chore: update file(s): main.go

--- main.go
+++ main.go
@@ -3,8 +3,8 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"log"
 	"os"
-  "log"
 )
 
 func main() {
@@ -22,4 +22,22 @@ func main() {
 	}
 
 	fmt.Printf("%+v\n", appVersions)
+
+	// Update the value of app1
+	appVersions["app1"] = "999"
+
+	// Print the updated map
+	fmt.Printf("Updated: %+v\n", appVersions)
+
+	// Optionally, you can write the updated map back to the file (if desired)
+	// To do this, you would marshal the map back to JSON and save it
+	updatedJsonData, err := json.MarshalIndent(appVersions, "", "  ")
+	if err != nil {
+		log.Fatal("Error during MarshalIndent(): ", err)
+	}
+
+	err = os.WriteFile("version.json", updatedJsonData, 0644)
+	if err != nil {
+		log.Fatal("Error writing updated file:", err)
+	}
 }
